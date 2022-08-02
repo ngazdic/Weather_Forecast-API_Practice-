797a6d1d57ca39373d7c8f4a7ae0b21d6fc186e8
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-02 17:39:06 -0400

    practice done

---
+++ Weather_Forecast/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+string APIKeys = File.ReadAllText("appsettings.json");
+
+string weatherAppKey = JObject.Parse(APIKeys).GetValue("WeatherAPI").ToString();
+
+
+string weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip=11206,US&appid={weatherAppKey}";
+var weatherJsonResponse = client.GetStringAsync(weatherURL).Result;
+
+
+
+var weatherObject = JObject.Parse(weatherJsonResponse);
+
+
+string description = weatherObject["weather"].First["description"].ToString();
+
+Console.WriteLine(description);
+
+double temp = double.Parse(weatherObject["main"]["temp"].ToString());
+
+Console.WriteLine(temp);
